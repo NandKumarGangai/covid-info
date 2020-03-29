c4a8fdc4fe1d950a068a6f07c22d3e7c5546a191
nandkumargangai1@outlook.com <nandkumargangai1@outlook.com>
2020-03-29 17:16:38 +0530

    Updates

---
+++ precache-manifest.80ea65a1f86762682a777138930de5d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f405c26fcdefad5e5ec7c8c8755730db",
+    "url": "/covid-info/index.html"
+  },
+  {
+    "revision": "730fc7751d36a15da04b",
+    "url": "/covid-info/static/css/main.77c7461d.chunk.css"
+  },
+  {
+    "revision": "2c593e364f33ed3248b0",
+    "url": "/covid-info/static/js/2.04dc2e94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-info/static/js/2.04dc2e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730fc7751d36a15da04b",
+    "url": "/covid-info/static/js/main.6852848e.chunk.js"
+  },
+  {
+    "revision": "0f5c142cd5272ad18356",
+    "url": "/covid-info/static/js/runtime-main.170b1f91.js"
+  }
+]);
