61776756cf0305f2380bbeeef55b2e49cf1b235f
Nandkumar Gangai <nandkumargangai1@outlook.com>
2021-05-25 19:36:23 +0530

    Update covidDataController.js

--- src/server/controllers/covidDataController.js
+++ src/server/controllers/covidDataController.js
@@ -1,7 +1,9 @@
 const covid = require('novelcovid');
 const path = require('path');
 const fs = require('fs');
-
+covid.settings({
+    baseUrl: 'https://disease.sh' | 'https://api.caw.sh' | 'https://corona.lmao.ninja'
+})
 const getAllData = async () => JSON.stringify(await covid.getAll());
 
 const getCountryData = async country => JSON.stringify(await covid.getCountry({ country }));
